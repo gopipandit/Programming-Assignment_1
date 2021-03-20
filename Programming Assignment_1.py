a317daaf44dd058663ea581c6db76c8b95e3ac79
Gopi Pandit <65672868+gopi1035@users.noreply.github.com>
2021-03-20 20:52:15 +0530

    Add files via upload

---
+++ Programming Assignment_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a program to print "Hello Python"
+print("Hello Python")
+
+
+# In[5]:
+
+
+#Write a Python program to do arithmetical operations addition and division.?
+a=4+5
+b=4/5
+print(a)
+print(b)
+
+
+# In[9]:
+
+
+#Write a Python program to find the area of a triangle?
+l= int(input("Please enter the Base-"))
+h= int(input("Please enter the Height-"))
+area =(l*h)/2
+print("The area of triangle is-{}".format(area))
+
+
+# In[12]:
+
+
+#Write a Python program to swap two variables?
+x= input("Please enter your age-")
+y= input("Please enter your Height-")
+print("Your age is {}.".format(y))
+print("Your height is {}.".format(x))
+
+
+# In[ ]:
+
+
+
+
